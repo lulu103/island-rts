e11657fee994a08a0aa6bd1642e688ca75283458
lulu103 <andylan.mobile@gmail.com>
2026-06-30 09:34:07 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,46 @@
-self.addEventListener('fetch',()=>{});
+const CACHE_NAME = "island-rts-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/game.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// Install and cache app files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+// Remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// Serve cached content first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
